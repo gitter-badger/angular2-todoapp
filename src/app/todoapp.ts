44ccd6cd319804bb3cbf4c478a2d7ed53e5edfa7
angular-cli <angular-cli@angular.io>
2015-11-06 21:07:40 +0100

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app/todoapp.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'todoapp-app',
+  providers: [],
+  templateUrl: 'app/todoapp.html',
+  directives: [],
+  pipes: []
+})
+export class TodoappApp {
+
+  constructor() {}
+
+}
