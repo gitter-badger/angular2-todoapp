10eb0b8838c11480594b13d507a698adbe08808a
Peekmo <axel.anceau@gmail.com>
2015-11-06 21:17:47 +0100

    Create todolist component

--- src/app/todoapp.ts
+++ src/app/todoapp.ts
@@ -1,11 +1,11 @@
 import {Component, bootstrap} from 'angular2/angular2';
-
+import {TodoList} from './components/todo-list/todo-list';
 
 @Component({
   selector: 'todoapp-app',
   providers: [],
   templateUrl: 'app/todoapp.html',
-  directives: [],
+  directives: [TodoList],
   pipes: []
 })
 export class TodoappApp {
